a42f288eab0830d1c01590e1fe727eb65a17df1a
shoronshaha <smaranshaha@gmail.com>
2022-05-18 23:36:49 +0600

    connect to the database and used to the post method for the user added his task and task store the mongodb database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -11,6 +12,28 @@ app.use(express.json());
 
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ffqd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const allTaskCollection = client.db('to-do-app').collection('task');
+
+
+        app.post('/addTask', async (req, res) => {
+            const addTask = req.body;
+            const result = await allTaskCollection.insertOne(addTask);
+            res.send(result);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
